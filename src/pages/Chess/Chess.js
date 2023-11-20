db0d003dbe60c64afc3f92eb96f01cd77f6c694a
SeanEABurke <sean.burke@live.ca>
2023-11-20 12:17:18 -0500

    chess music mode selector is working

--- src/pages/Chess/Chess.js
+++ src/pages/Chess/Chess.js
@@ -1,13 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { playChessMusic, setup } from "../../services/ChessMusic";
+import { Modes } from "../../services/Scales";
+import Card from "react-bootstrap/Card";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 const Chess = () => {
+  const [chessMoves, setChessMoves] = useState("");
+  const [selectedMode, setSelectedMode] = useState("");
+
+  const handleChessMovesChange = (event) => {
+    setChessMoves(event.target.value);
+  };
+
+  const handleModeChange = useCallback((mode) => {
+    setSelectedMode(mode);
+    console.log(mode);
+  }, []);
+
+  const handlePlayChessMusic = async () => {
+    await playChessMusic(chessMoves, selectedMode);
+  };
+
   return (
-    <div>
-      <button onClick={playChessMusic} className="chord-btn">
-        Play
-      </button>
-    </div>
+    <>
+      <div>
+        <button onClick={handlePlayChessMusic} className="chord-btn">
+          Play
+        </button>
+        <input
+          type="text"
+          value={chessMoves}
+          onChange={handleChessMovesChange}
+        ></input>
+      </div>
+      <Card
+        bg="dark"
+        className="rounded chordCard"
+        style={{ width: "18rem" }}
+        key="modes"
+      >
+        <CardHeader className="quiz-header">
+          <label className="container">
+            <span className="text-decoration-underline ">Modes</span>
+          </label>
+        </CardHeader>
+        {Object.keys(Modes).map((mode) => (
+          <div key={mode}>
+            <label className="container">
+              <input
+                type="checkbox"
+                id={mode}
+                value={mode}
+                checked={mode === selectedMode}
+                onChange={() => handleModeChange(mode)}
+              />
+              <div className="checkmark"></div>
+              <span>{mode}</span>
+            </label>
+          </div>
+        ))}
+      </Card>
+    </>
   );
 };
 
--- src/services/ChessMusic.js
+++ src/services/ChessMusic.js
@@ -1,16 +1,5 @@
 import * as Tone from "tone";
-
-// Define a mapping of chess moves to musical intervals
-const chessToMusic = {
-  a: "A4",
-  b: "B4",
-  c: "C4",
-  d: "D4",
-  e: "E4",
-  f: "F4",
-  g: "G4",
-  h: "G3",
-};
+import { Modes } from "./Scales";
 
 const chessToRhythm = {
   1: "16n",
@@ -23,6 +12,29 @@ const chessToRhythm = {
   8: "2n",
 };
 
+const groupings = {
+  rhythmsA: {
+    1: 4,
+    2: 3,
+    3: 2,
+    4: 1,
+    5: 1,
+    6: 2,
+    7: 3,
+    8: 4,
+  },
+  rhythmsB: {
+    1: 1,
+    2: 1,
+    3: 2,
+    4: 2,
+    5: 3,
+    6: 3,
+    7: 4,
+    8: 4,
+  },
+};
+
 function chessNotationToArray(chessGame) {
   // Step 1: Split the string based on spaces
   const movesArray = chessGame.split(/\s+/);
@@ -32,90 +44,143 @@ function chessNotationToArray(chessGame) {
 
   // Step 3: Remove non-alphanumeric characters
   const cleanedMoves = filteredMoves.map((move) =>
-    move.replace(/[^a-zA-Z0-9]/g, "")
+    move.replace(/[^a-zA-Z1-9]/g, "")
   );
 
   return cleanedMoves;
 }
 
 const synth = new Tone.Synth().toDestination();
+let melodySequence = "";
 
-export function playChessMusic() {
-  Tone.start().then(() => {
-    const chessGame =
-      "1. e4 c6 2. d4 d5 3. Nc3 e6 4. f4 Nd7 5. e5 f6 6. Bd3 Bb4 7. Qh5+ Kf8 8. Ne2 Ne7 9. O-O f5 10. Ng3 g6 11. Qh6+ Kf7 12. Nce2 Ng8 13. Qh3 Be7 14. c4 h5 15. cxd5 cxd5 16. Nc3 a6 17. Be2 b5 18. Kh1 Nb6 19. b3 Qc7 20. Bd2 Bd7 21. Nxh5 gxh5 22. Bxh5+ Kf8 23. g4 Be8 24. Qf3 b4 25. Bxe8 bxc3 26. Rac1 Rxe8 27. Rxc3 Qa7 28. gxf5 exf5 29. Rc6 Nd7 30. Qxd5";
-    const chessMoves = chessNotationToArray(chessGame);
-    console.log(chessMoves);
-    const lowercaseMoves = chessMoves.map((move) =>
-      move
+export const playChessMusic = async (chessGame, mode) => {
+  await Tone.start();
+
+  Tone.Transport.stop();
+  // const chessGame =
+  //   "1. e4 d5 2. f3 dxe4 3. fxe4 Nc6 4. Nc3 Nf6 5. Bb5 Bg4 6. Nge2 Qd6 7. d3 a6 8. Ba4 O-O-O 9. Bf4 Qb4 10. Bxc6 bxc6 11. O-O e6 12. Bg5 h6 13. Bxf6 gxf6 14. Rxf6 Rd7 15. Qf1 Bh5 16. Rb1 Rg8 17. Nf4 Bg6 18. Nxg6 fxg6 19. a3 Qc5+ 20. Kh1 Qe5 21. Rxf8+ Rxf8 22. Qxf8+ Kb7 23. Na4 h5 24. Nc5+ 1-0";
+  const chessMoves = chessNotationToArray(chessGame);
+  console.log(chessMoves);
+
+  const subarrays = [];
+
+  // Iterate through the chessMoves array
+  let index = 0;
+  while (index < chessMoves.length) {
+    // Get the number from the current move
+    try {
+      const currentNumber = parseInt(chessMoves[index].match(/\d+/)[0]);
+      console.log(currentNumber);
+      // Create a subarray with the next 'currentNumber' elements
+      if (currentNumber !== 0) {
+        const currentSubarray = chessMoves.slice(
+          index,
+          index + groupings.rhythmsA[currentNumber]
+        );
+        // Push the current subarray to the array of subarrays
+        subarrays.push(currentSubarray);
+
+        // Move the index to the next move after the current subarray
+        index += groupings.rhythmsA[currentNumber] + 1;
+      } else {
+        index++;
+      }
+    } catch {
+      console.log("Something was wrong");
+      index++;
+    }
+  }
+
+  console.log(subarrays);
+
+  const filteredSubarrays = subarrays.map((subarray) =>
+    subarray.map((note) =>
+      note
         .split("")
         .filter((char) => /[a-z]/.test(char))
         .join("")
-    );
+    )
+  );
 
-    const melody = lowercaseMoves.map(
-      (move) => chessToMusic[move.toLowerCase()]
-    );
-    console.log(melody);
-    const numbers = chessMoves.map((move) =>
-      move
-        .split("")
-        .filter((char) => /[1-8]/.test(char))
-        .join("")
-    );
-    const rhythms = numbers.map((move) => chessToRhythm[move]);
-    console.log(rhythms);
+  console.log(filteredSubarrays);
+  console.log(mode);
 
-    const melodyWithDurations = melody.map((note, index) => ({
-      note,
-      duration: rhythms[index % rhythms.length],
-    }));
-    console.log(melodyWithDurations);
+  const melody = filteredSubarrays.map((move) =>
+    move.map((note) => Modes[mode][note[note.length - 1]])
+  );
+  console.log(melody);
 
-    // Can't get this part to make sound
-    //     const filteredMelody = melodyWithDurations.filter(
-    //       (value) => value.note && value.duration
-    //     );
+  if (melodySequence) {
+    melodySequence.stop();
+    melodySequence.dispose();
+  }
 
-    //     console.log(filteredMelody);
+  // Create a sequence to play the melody
+  melodySequence = new Tone.Sequence(
+    (time, note) => {
+      // Trigger the synth to play the note at the specified time
+      synth.triggerAttackRelease(note, 1, time);
+    },
+    melody,
+    0.7
+  );
 
-    //     Tone.Transport.start();
-    //     console.log("Transport state:", Tone.Transport.state);
+  // Start the Tone.js transport
+  Tone.Transport.start();
 
-    //     const part = new Tone.Part((time, value) => {
-    //       console.log(value.note);
-    //       if (value && value.note && value.duration) {
-    //         synth.triggerAttackRelease(value.note, value.duration, time);
-    //         console.log(value.note);
-    //       } else {
-    //         console.error("Invalid value in melodyWithDurations:", value);
-    //       }
-    //     }, filteredMelody).start();
+  // Start the melody sequence
+  melodySequence.start(0);
 
-    // Create a sequence to play the melody
-    const melodySequence = new Tone.Sequence(
-      (time, { note, duration }) => {
-        // Trigger the synth to play the note at the specified time
-        synth.triggerAttackRelease(note, duration, time);
-      },
-      melodyWithDurations.map(({ note, duration }) => ({ note, duration }))
-    );
+  //   Schedule a stop event after the duration of the melody
+  const melodyDuration = melody.length * 0.7;
+  Tone.Transport.scheduleOnce(() => {
+    // Stop the Tone.js transport
+    Tone.Transport.stop();
+  }, `+${melodyDuration}`);
+};
 
-    // Start the Tone.js transport
-    Tone.Transport.start();
+// Can't get this part to make sound
+//     const filteredMelody = melodyWithDurations.filter(
+//       (value) => )value.note && value.duration
+//     );
 
-    // Start the melody sequence
-    melodySequence.start(Tone.now());
+//     console.log(filteredMelody);
 
-    //   Schedule a stop event after the duration of the melody
-    const melodyDuration = melodyWithDurations.reduce(
-      (totalDuration, { duration }) =>
-        totalDuration + Tone.Time(duration).toSeconds(),
-      0
-    );
-    Tone.Transport.scheduleOnce(() => {
-      // Stop the Tone.js transport
-      Tone.Transport.stop();
-    }, `+${melodyDuration}`);
-  });
-}
+//     Tone.Transport.start();
+//     console.log("Transport state:", Tone.Transport.state);
+
+//     const part = new Tone.Part((time, value) => {
+//       console.log(value.note);
+//       if (value && value.note && value.duration) {
+//         synth.triggerAttackRelease(value.note, value.duration, time);
+//         console.log(value.note);
+//       } else {
+//         console.error("Invalid value in melodyWithDurations:", value);
+//       }
+//     }, filteredMelody).start();
+
+// const lowercaseMoves = subarrays.map((move) =>
+//   move
+//     .split("")
+//     .filter((char) => /[a-z]/.test(char))
+//     .join("")
+// );
+
+// const melody = lowercaseMoves.map(
+//   (move) => chessToMusic[move.toLowerCase()]
+// );
+// console.log(melody);
+// const numbers = chessMoves.map((move) =>
+//   move
+//     .split("")
+//     .filter((char) => /[1-8]/.test(char))
+//     .join("")
+// );
+// const rhythms = numbers.map((move) => chessToRhythm[move]);
+// console.log(rhythms);
+
+// const melodyWithDurations = melody.map((note, index) => ({
+//   note,
+//   duration: rhythms[index % rhythms.length],
+// }));
+// console.log(melodyWithDurations);
